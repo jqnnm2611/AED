6b60b3a53a3c337eaa4fa47dd024b35872b9f2c6
Joaquín Núñez <joaquin.nunez@ucsp.edu.pe>
2026-09-02 22:18:25 -0500

    Update util.cpp

--- util.cpp
+++ util.cpp
@@ -16,13 +16,18 @@ struct util {
 		}
 	}
 	void merge(int* ini, int* mid, int* fin) {
-		while (mid < fin) {
+		while (mid <= fin) {
 			if (*ini <= *mid) ini++;
 			else {
 				for (int* p = mid; p > ini; p--) swap(*p, *(p - 1));
+				ini++;
+				mid++;
 			}
-			mid++;
 		}
+	}
+	void print(int* ini, int* fin){
+	    for (int* p = ini; p <= fin; p++) cout << *p << ' ';
+	    cout << endl;
 	}
 };
 
@@ -36,12 +41,11 @@ public:
 int main() {
 
 	int arr[10] = {1,4,7,8,21,3,4,5,6,9};
-	for (int* p = arr; p < arr + 10; p++) cout << *p << ' ';
-	cout << endl;
 	util<div3> u;
+	
+	u.print(arr, arr+9);
 	//u.split(arr, arr + 9);
 	u.merge(arr, arr+5, arr+9);
-
-	for (int* p = arr; p < arr + 10; p++) cout << *p << ' ';
+	u.print(arr, arr+9);
 
 }
